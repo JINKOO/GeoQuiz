1e05fc0c8d2658e50ed545c94d87c6af9f2f255f
JINKOO <jacob777300@gmail.com>
2021-11-21 18:40:27 +0900

    data 생성 메소드 추가

--- app/src/main/java/com/kjk/geoquiz/data/QuestionModel.kt
+++ app/src/main/java/com/kjk/geoquiz/data/QuestionModel.kt
@@ -1,7 +1,20 @@
 package com.kjk.geoquiz.data
 
-import com.kjk.geoquiz.R
+
+/**
+ *  비즈니스 로직을 담당하는 부분.
+ *  Model 컴포넌트에 속한다.
+ *  Context속성이 포함되면 안된다.
+ */
 
 class QuestionModel {
 
+    private val questionList = ArrayList<QuestionEntity>()
+
+    /**
+     * 질문 리스트를 생성하는 메소드
+     */
+    fun createQuestionList() {
+
+    }
 }
