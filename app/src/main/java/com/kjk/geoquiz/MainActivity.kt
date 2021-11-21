a18821695de86213ca02fb84c63e27dff1f7bbbf
JINKOO <jacob777300@gmail.com>
2021-11-21 18:55:41 +0900

    컨트롤러(MainActivity)에 여러 개 질문을 생성하고 보여주는 로직 추가

--- app/src/main/java/com/kjk/geoquiz/MainActivity.kt
+++ app/src/main/java/com/kjk/geoquiz/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Gravity
 import android.view.View
 import android.widget.Toast
+import com.kjk.geoquiz.data.QuestionEntity
 import com.kjk.geoquiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -13,6 +14,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private val questionLists = listOf(
+            QuestionEntity(R.string.question_australia, true),
+            QuestionEntity(R.string.question_ocean, true),
+            QuestionEntity(R.string.question_mideast, false),
+            QuestionEntity(R.string.question_africa, false),
+            QuestionEntity(R.string.question_americas, true),
+            QuestionEntity(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -20,6 +32,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding.trueButton.setOnClickListener(this)
         binding.falseButton.setOnClickListener(this)
         binding.nextButton.setOnClickListener(this)
+
+        updateQuestion()
     }
 
     override fun onClick(v: View?) {
@@ -32,8 +46,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             binding.nextButton -> {
                 //TODO 다음 질문
+                currentIndex = (currentIndex + 1) % questionLists.size
+                updateQuestion()
             }
         }
+    }
+
+    private fun updateQuestion() {
+        binding.questionTextView.setText(questionLists[currentIndex].textResId)
     }
 
     private fun showToast(isAnswer: Boolean) {
